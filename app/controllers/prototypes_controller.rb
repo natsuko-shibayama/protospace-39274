5da4d9a7eebac3fc4179c565d57863d65b579bb3
natsuko-shibayama <124324097+natsuko-shibayama@users.noreply.github.com>
2023-05-12 16:57:02 +0900

    入力済みの項目の保持

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -15,6 +15,7 @@ class PrototypesController < ApplicationController
     if @prototype.save
       redirect_to root_path
     else
+      @prototype = Prototype.new(prototype_params)
       render :new
     end
   end
